fbae3515178d7f0cbf672ec369df25397475d10c
NandiniBorase <borase.nandini@gmail.com>
2023-01-12 13:40:22 +0530

    first code committed

---
+++ src/com/test/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("nandini");
+		al.add("pranit");
+		al.add("pralhad");
+		
+		System.out.println(al);
+	}
+
+}
